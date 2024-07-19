99ee724d66125e8937fc0042787ef50b3cb55c0e
Brad Ball <brad.ball@twinspires.com>
2024-07-19 12:57:34 -0400

    Initial Commit

---
+++ AI Navigation Demo/AI_Navigation_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AI_Navigation_DemoApp.swift
+//  AI Navigation Demo
+//
+//  Created by Bradley Ball on 7/19/24.
+//
+
+import SwiftUI
+
+@main
+struct AI_Navigation_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AI Navigation Demo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AI Navigation Demo
+//
+//  Created by Bradley Ball on 7/19/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
